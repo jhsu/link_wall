317ef29153af532cb66375f0fa230f59dda90138
Joseph Hsu <jhsu.x1@gmail.com>
2009-12-04 21:53:24 -0500

    try fixing for heroku

--- lib/models/group.rb
+++ lib/models/group.rb
@@ -17,6 +17,10 @@ class Group < ActiveRecord::Base
   end
 
   def clicks_by_day
-    self.clicks.count('clicks.id', :group => "strftime('%Y-%m-%d', clicks.created_at)").map {|date, count| count }
+    if (ENV['DATABASE_URL'])
+      self.clicks.count('clicks.id', :group => "to_char(clicks.created_at, 'YYYY-MM-DD')").map {|date, count| count }
+    else
+      self.clicks.count('clicks.id', :group => "strftime('%Y-%m-%d', clicks.created_at)").map {|date, count| count }
+    end
   end
 end
